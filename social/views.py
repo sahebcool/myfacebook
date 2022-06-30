d67e6b403301cb30fde5ad7bfa0d9e87c1654c48
sahebcool <amit.saheb.143@gmail.com>
2022-06-30 17:05:06 +0530

    third commit

--- social/views.py
+++ social/views.py
@@ -1,12 +1,10 @@
-from importlib.resources import contents
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 
 from django.contrib.auth.models import User
 
 from django.contrib.auth.decorators import login_required
-from grpc import Status
-from requests import post
 
 from social.models import Userprofile
 
